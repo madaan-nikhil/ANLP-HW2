4541311ce51dd79e4828d9cc2870b50a2053d05d
Shubhranshu Singh <shubhranshu.singh@alumni.iitgn.ac.in>
2022-10-15 15:31:31 -0400

    Preprocess PDF init

---
+++ setup.sh
@@ -0,0 +1,6 @@
+pip install git+https://github.com/titipata/scipdf_parser
+pip install beautifulsoup4
+python -m spacy download en_core_web_sm
+
+bash serve_grobid.sh
+# Set JAVA_HOME -- Java installation

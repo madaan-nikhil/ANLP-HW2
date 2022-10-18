a77d9e0bfc1fc9817148d8365e56b0d02a23fa52
ShubhranshuSingh <shubhranshu.singh@alumni.iitgn.ac.in>
2022-10-18 21:45:11 +0000

    Changes for conference papers

--- preprocess.py
+++ preprocess.py
@@ -1,41 +1,70 @@
+import glob
+import json
 import os
 import pickle
+import random
 import re
 import scipdf
 from spacy.lang.en import English
 import tqdm
+random.seed(42)
 
 
 nlp = English()
 tokenizer = nlp.tokenizer
 
-
-with open('pdf_urls_100.pkl', 'rb') as f:
-    pdf_urls = pickle.load(f)
-    
 os.makedirs('data', exist_ok = True)
-start_idx = 70
+os.makedirs('data/filtered', exist_ok = True)
+
+pdf_urls = []
+paths = glob.glob('conference_papers/*')
+
+for path in paths:
+    with open(path, 'rb') as f:
+        urls = pickle.load(f)
+        
+    urls = random.sample(urls, 20)
+    with open('data/filtered/' + path[path.find('/') + 1:], 'wb') as f:
+        pickle.dump(urls, f)
+        
+    pdf_urls = pdf_urls + urls
+
+start_idx = 119
+
+all_papers = []
+all_meta = []
 
 for url in tqdm.tqdm(pdf_urls[start_idx:]):
     try:
-      article_dict = scipdf.parse_pdf_to_dict(url + '.pdf')
+        article_dict = scipdf.parse_pdf_to_dict(url)
     except:
-      print(f'Could not parse {url}')
-      continue
+        print(f'Could not parse {url}')
+        continue
     meta = {}
     for key in ['authors', 'pub_date', 'title']:
         meta[key] = article_dict[key]
+    meta['url'] = url
         
     text = [article_dict['abstract']]
     for section in article_dict['sections']:
         text.append(section['text'])
         
     url_path = '_'.join(url.split('/'))
-    with open(f'data/{url_path}.tokens', 'wb') as f:
-        full_text = ' '.join(text)
-        full_text = re.sub('\n', '', full_text)
-        tokenized = tokenizer(full_text)
-        pickle.dump(tokenized, f)
+    
+    full_text = ' '.join(text)
+    full_text = re.sub('\n', '', full_text)
+    tokenized = tokenizer(full_text)
+    full_text = ""
+    for token in tokenized:
+        full_text = full_text + str(token) + " "
+    full_text = full_text[:-1]
+    
+    all_papers.append({'text': full_text, 'paper_id': url_path})
+    all_meta.append(meta)
+    
         
-    with open(f'data/{url_path}.meta', 'wb') as f:
-        pickle.dump(meta, f)
+with open(f'data/data.json', 'w') as f:
+    json.dump(all_papers, f)
+
+with open(f'data/meta.pkl', 'wb') as f:
+    pickle.dump(all_meta, f)
--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
+sudo apt-get update
+sudo apt-get install python3
+sudo apt install python3-pip
+sudo apt-get install unzip
+sudo apt install ipython3
+
+pip install jupyterlab
+pip install markupsafe==2.0.1
+pip install gdown
 pip install git+https://github.com/titipata/scipdf_parser
 pip install beautifulsoup4
 python3 -m spacy download en_core_web_sm
 
 sudo apt install openjdk-8-jdk
 # export JAVA_HOME=path-to-java
-bash serve_grobid.sh
+gdown https://drive.google.com/drive/folders/17XrzUfgdoCyIMwFd5yd6XkBXPaWxhyO2 -O conference_papers --folder
+# bash serve_grobid.sh
+

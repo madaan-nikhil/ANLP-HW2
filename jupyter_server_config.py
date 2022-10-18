02c76ac10076213dcd88a06925341e4a205d14f5
ShubhranshuSingh <shubhranshu.singh@alumni.iitgn.ac.in>
2022-10-18 21:45:11 +0000

    Changes for conference papers

---
+++ jupyter_server_config.py
@@ -0,0 +1,4 @@
+c.ServerApp.ip = '*' # bind to any network interface
+c.ServerApp.password = u'sha256:e0b89b791f0d:f1fd25529fd4eec002f48a284fb120a2ab3be38f84c90f024cde145ead443033'
+c.ServerApp.open_browser = False
+c.ServerApp.port = 8888 # or any other ports you'd like

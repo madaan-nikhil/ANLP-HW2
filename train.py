c27b3bec229add3f64cd7c165a1250dba3aa1a29
madaan-nikhil <nikhilm29.09@gmail.com>
2022-10-22 18:38:07 -0400

    Model Trainer

---
+++ train.py
@@ -0,0 +1,89 @@
+import argparse
+import os
+from torch.optim import AdamW
+from tqdm.auto import tqdm
+import torch
+import numpy as np
+import pandas as pd
+from transformers import AutoModel, AutoTokenizer
+from trainer import *
+from model import *
+
+
+def _load_model(model_dir, filename, model, device):
+    """
+    Load the model checkpoint
+    
+    Args:
+    - model_dir: checkpoints local directory
+    - filename: name of the checkpoint file
+    - rest: args used for creating the model instance
+    """
+    print(f'Loading model :{filename} {model_dir}')
+    with open(os.path.join(model_dir, filename), 'rb') as f:
+        model.load_state_dict(torch.load(f))
+    model.to(device)
+    return model
+
+
+if __name__ == '__main__': 
+    
+    NUM_LABELS = ?
+    
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument('--epochs', type=int, default=50, help="num of epochs")
+    parser.add_argument('--batch_size', type=int, default=32, help="batch size")
+    parser.add_argument('--lr', type=float, default=0.00001, help="learning rate")
+    parser.add_argument('--use-cuda', type=bool, default=False)# not used
+    parser.add_argument('--resume', type=int, default=0, help="resume training")
+    parser.add_argument('--resume_from_epoch', type=int, default=0, help="checkpoint to resume")
+    parser.add_argument('--training', type=int, default=1, help='Flag to train the model')
+    parser.add_argument('--inference', type=int, default=0, help='inference flag')
+    parser.add_argument('--data_dir', help="data directory")
+    parser.add_argument('--model_dir', help="model directory")
+    parser.add_argument('--model_file', help="model_file")
+    parser.add_argument('--model_name', type=str, default="allenai/scibert_scivocab_cased", help="resume training")
+
+    args, _ = parser.parse_known_args()
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+    print(f'Loading data')
+    train, val, test = # get dataloaders
+    print(f'Data Loaded')
+    
+    model_name = args.model_name 
+    # Download pytorch model
+    feature_extractor = AutoModel.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = TokenClassification(feature_extractor,
+                                device,
+                                NUM_LABELS,
+                                feature_extractor.config.hidden_size,
+                                dropout=0.1)
+    
+    if args.resume:
+        model = _load_model(args.model_dir, filename=args.model_file, model=model, device=device)
+    
+    
+    train_loader = # initialize train loader
+    dev_loader = # initialize val loader
+    
+    optimizer = AdamW(model.parameters(), lr=args.lr)
+    schedular = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, factor=0.5, min_lr=1e-8)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    print("Model Training begins")
+    if args.training:
+        model = model.to(device)
+        trainer = Trainer(model,
+                        args.epochs,
+                        optimizer,
+                        scheduler,
+                        criterion,
+                        train_loader,
+                        dev_loader,
+                        device,
+                        args.model_dir)
